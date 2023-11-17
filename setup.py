4fe72fd245184e93d8048f961489bdeb6b193dee
Erik Rosvall <erirosv@gmail.com>
2023-11-17 22:14:03 +0100

    the linking works

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-from setuptools import setup, find_packages
-
-#setup(name='datascience', version='0.1', packages=find_packages())
-
 #!/usr/bin/env python3
 
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 directory = Path(__file__).resolve().parent
 with open(directory / 'README.md', encoding='utf-8') as f:
@@ -19,11 +15,6 @@ setup(
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    #packages=['data_helpers', 'utils'],
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-    ],
     include_package_data=True
 )
