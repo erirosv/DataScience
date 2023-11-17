7a35fd32771e6b86114bd990e19153fd19149a0c
Erik Rosvall <erirosv@gmail.com>
2023-11-17 22:53:28 +0100

    starting to get the project going

--- data_helpers/pre_data.py
+++ data_helpers/pre_data.py
@@ -1,3 +1,11 @@
-def look_nan(df):
+def look_for_nan(df):
+    print('------------------------------------')
     print(f"File containd NaN-values: {df.isnull().values.any()}")
-    print(df.head())
+    print('------------------------------------')
+
+def info(df, column_name: str) -> None:
+    print(f'Column names: {df.columns}')
+    print('---------------------------')
+    print('\tDescription')
+    print('---------------------------')
+    print(df[column_name].describe())
