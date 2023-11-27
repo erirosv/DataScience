114f3f3a3a09c6800f2fc717e31d6db8ebe9825f
Erik Rosvall <erirosv@gmail.com>
2023-11-27 14:49:53 +0100

    added the forwardpass, and the code for checking for the supported accelerator

--- kaggle/Santander_Customer_Transaction_Prediction_Dataset/training.py
+++ kaggle/Santander_Customer_Transaction_Prediction_Dataset/training.py
@@ -9,6 +9,10 @@ from tqdm import tqdm
 from data import get_tensordata
 
 class Net(nn.Module):
+    '''
+    A simple neural network based on troch. There is no backpropagation 
+    at the moment, may come at a later date.
+    '''
     def __init__(self, input_size, hidden_dimentions):
         super(Net, self).__init__()
         self.batch_norm = nn.BatchNorm1d(input_size)
@@ -16,4 +20,18 @@ class Net(nn.Module):
         self.layer_2 = nn.Linear(input_size // 2 * hidden_dimentions, 1)
 
     def forward(self, x):
-        pass
+        x = self.batch_norm(x)
+        N = x.shape[0]
+        # set the shape (N, 200, 1)
+        original_features = x[:, :200].unsqueeze(2)
+        new_features = x[:, :200].unsqueeze(2)
+
+        x = torch.cat([original_features, new_features], dim=2)
+        x = F.relu(self.layer_1(x)).reshape(N, -1)
+        x = torch.sigmoid(self.layer_2(x)).view(-1)
+        return x
+
+DEVICE = 'cpu'
+if torch.cuda.is_available(): DEVICE = 'cuda'
+elif torch.backends.mps.is_available(): DEVICE = 'mps'
+else: DEVICE
